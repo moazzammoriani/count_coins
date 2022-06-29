58a864140bbe23983fad965e3c3cc0f11f39d506
Moazzam Moriani <mozmoriani@gmail.com>
2022-06-29 16:20:41 +0500

    Actual first commit

--- count_change_iter.ml
+++ count_change_iter.ml
@@ -1,34 +1,120 @@
 module L = List
+module A = Array
 
 let sum lst = L.fold_left (+) 0 lst
 
-let rec iter (amt : int) (coins : (int * int) list) (curr : int list) (acc : int list list) =
+type amt = int
+type den = int
+type den_qty = int
+
+let rec next_deductible coins amt = 
+    match coins with
+    | [] -> 0
+    | (den, qty)::xs -> if amt - den > 0 then den else next_deductible xs amt
+
+
+let rec iter (amt : amt) (coins : (den * den_qty) list) (curr : int list) (acc : (den * amt * den list) list) : (den * amt * den list) list =
     match amt, coins with
     | 0, coins -> acc 
-    | _, [] -> []
+    | _, [] -> acc
     | _, (den,qty)::rst-> begin
         let new_amt = amt - den in
+        let new_coins = (den, (qty-1))::rst in  
+        let new_curr = den::curr in
+        let curr_den = fst @@ L.hd @@ coins in
+        (*let get_next_den coins = 
+            let nd = if (L.tl coins) = [] then 0  else (fst (L.hd @@ L.tl coins)) in
+            if (new_amt - curr_den) < 0 then 
+                if (L.length @@ coins > 1) then 
+                    (fst @@ (L.nth coins 1)) 
+                else 0
+            else nd in  *)
+        (*let next_d = get_next_den coins in*)
         if qty == 1 then 
             if den > amt then begin
                 iter amt rst curr acc
-            end else 
-                iter new_amt rst curr acc
+            end else begin
+                (*let next_d = get_next_den rst in*)
+                iter new_amt rst new_curr ((curr_den, new_amt, new_curr)::acc) end
         else if den > amt then begin
             iter amt rst curr acc
         end else if (amt - den) < 0 then
             acc
         else
-            let new_coins = (den, (qty-1))::rst in  
-            let new_curr = den::curr in
-            iter new_amt new_coins new_curr (new_curr::acc)
+            let curr = if new_amt < curr_den then next_deductible coins new_amt else curr_den in
+            iter new_amt new_coins new_curr ((curr, new_amt, new_curr)::acc)
     end
 
-let rec collect_compleleted_enum (amt : int) coins enums =
+let it amt coins curr = iter amt coins curr []
+
+
+let get_1 (x,y,z) = x 
+
+let get_2 (x,y,z) = y
+
+let get_3 (x,y,z) = z
+
+let filter data n = 
+    L.map get_3
+    (L.filter (fun t -> let x = get_1 t and y = get_2 t in if x > n && y - n > 0 then true else false) data)
+
+
+let rec get_cmp_enum (enums : (den * amt * den list) list) : den list list =
     match enums with
     | [] -> []
-    | _ -> L.hd enums
+    | (_, 0, y)::tail -> [y]
+    | _ -> []
 
-let rec f_rmng (amt : int) (coins : (int * int) list) (enums : int list list) : int list list =
+let index_of_den (coins : (den * den_qty) list) (den : den) = 
+    let rec aux (count:int) (coins : (den * den_qty) list) =
+        match coins with
+        | [] -> count
+        | _ -> aux (count + 1) (L.tl coins) in
+    aux 0 coins
+
+let den_of_index (coins : (den * den_qty) list) (index: int) = fst (L.nth (L.rev coins) index)
+
+let index_to_last (coins : (den * den_qty) list) (index: int) =
+    let rec aux count coins = 
+        match coins, count with 
+        | [],_ -> []
+        | _,0 -> coins
+        | _ -> aux (count - 1) (L.tl coins) in
+    aux index coins
+
+let f = ref (Array.init 4 (fun _ -> []))
+
+
+let itrandfltr amt coins seq dindex flist_ref= 
+    let doi i = den_of_index coins i in
+    let enums = ref [] in
+    let coins_len = L.length coins in
+    let root_to_leaf = it amt coins seq in
+    enums := L.append (get_cmp_enum root_to_leaf) !enums;
+    for j = dindex to (coins_len-1) do 
+        !flist_ref.(coins_len -1 - j) <- L.append (filter root_to_leaf (doi j)) !flist_ref.(coins_len -1 -j);
+    done;
+    !flist_ref
+    (* initialize array for filtered enums for each den
+    (* *)
+
+    for i = 0 to (l.length) do 
+        (* computer iter for denom i*)
+        (* filter the results of iter and add to corresponding list for each respective denom*)
+    done; 
+    for i = 1 to (L.length coins) do 
+        
+        for j = i to (L.length coins) do 
+            (* compute iter for den j for each item in the filtered list of den j*)
+            (* filter results from the iteration and place in the corresponding filtered list for its respective denom*)
+            
+        done
+    done;
+    enums*)
+
+
+
+(*let rec f_rmng (amt : int) (coins : (int * int) list) (enums : int list list) : int list list =
     let current_den = (fst (L.hd coins)) in
     let remaining_coins = L.tl coins in
     L.filter (fun x ->
@@ -36,11 +122,11 @@ let rec f_rmng (amt : int) (coins : (int * int) list) (enums : int list list) : 
             let total_value = (sum (L.map (fun (x,y) -> x*y))) in
             let rem_amt = amt - (sum x) in 
             if (rem_amt - total_value) <= 0
-                true
+                true*)
     
 
 
-let get_enums amt coins = 
+(*let get_enums amt coins = 
     let acc = ref [] in
     let filtered_for_remaining_dens = ref [] in
     let filtered_sub_tree_for_next_den = ref [] in
@@ -58,5 +144,5 @@ let get_enums amt coins =
         done
     done;
     end;
-    enums
+    enums*)
 
